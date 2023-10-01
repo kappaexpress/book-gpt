b3da74df3fd804edb55818503b8e630c9d60de13
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2023-10-01 16:43:35 +0000

    結合しワードファイルに出力する

---
+++ join_summary.py
@@ -0,0 +1,42 @@
+import docx
+import pandas as pd
+
+
+if __name__ == '__main__':
+    # tmp/index.csvを読み込む
+    df = pd.read_csv('tmp/index.csv')
+
+    # docxを新規作成
+    doc = docx.Document()
+
+    # loop_split_rangeを初期化する
+    loop_split_range = df.at[0, 'split_range']
+
+    # docxにsplit_rangeを追加する
+    doc.add_paragraph(loop_split_range)
+
+    # dfをforループで処理する
+    for index, row in df.iterrows():
+        # summaryから呼び込むためのファイルパスに変更する
+        summary_path = row['file_name'].replace('prompt', 'summary')
+
+        # summaryを読み込む
+        with open(summary_path) as f:
+            summary = f.read()
+        
+        # その行のsplit_rangeがloop_split_rangeと異なる場合
+        if row['split_range'] != loop_split_range:
+            # docxに改ページを追加する
+            doc.add_page_break()
+            
+            # loop_split_rangeを更新する
+            loop_split_range = row['split_range']
+
+            # docxにsplit_rangeを追加する
+            doc.add_paragraph(row['split_range'])
+        
+        # docxにsummaryを追加する
+        doc.add_paragraph(summary)
+    
+    # docxを保存する
+    doc.save('tmp/summary.docx')
