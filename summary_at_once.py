9e7a19af5785d340c9b702cdacce5bc887f2c816
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2023-11-12 14:15:38 +0000

    Add function to write summary to file

--- summary_at_once.py
+++ summary_at_once.py
@@ -3,7 +3,8 @@ import settings
 import key
 
 openai.api_key = key.api_key
-
+prompt_path = "prompt"
+summary_path = "summary"
 
 # 通常モデルを使用してテキストを生成する関数
 def generate_text(prompt):
@@ -32,11 +33,24 @@ def generate_text_instruct(prompt):
     return response['choices'][0]['text']
 
 
+# ファイル名に基づいて書き込みを行う関数
+def write_file(file_name: str, summary: str):
+    # ファイル名を作成　prompt_0000.txt -> summary_0000.txt
+    file_name = file_name.replace(prompt_path, summary_path)
+
+    with open(summary_path + "/" + file_name, "w") as f:
+        f.write(summary)
+
+
 if __name__ == "__main__":
-    # prompt/prompt_1.txtの内容を読み込みます。
-    with open("prompt/prompt_0006.txt") as f:
+    path = "prompt/prompt_0025.txt"
+    with open(path) as f:
         prompt = f.read()
 
     response = generate_text(prompt)
 
     print(response)
+
+    file_name = path.split("/")[-1]
+
+    write_file(file_name, response)
