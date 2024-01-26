e9dd547b6aec6876b7e1220e29a907c27d6d34cf
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2024-01-26 05:01:19 +0000

    Update file paths and filter data in search_split_point_candidate.py

--- search_split_point_candidate.py
+++ search_split_point_candidate.py
@@ -2,13 +2,14 @@ import pickle
 import os
 import pandas as pd
 import settings
+from scipy.stats import trim_mean
 
 
 # データの中身を確認する関数
 def check_df(df: pd.DataFrame) -> None:
     # check_df_page(df, settings.first_page - settings.page_num_adjustment)
     # check_df_page(df, settings.last_page - settings.page_num_adjustment)
-    check_df_page(df, 510)
+    check_df_page(df, 303)
 
     exit()
 
@@ -16,8 +17,8 @@ def check_df(df: pd.DataFrame) -> None:
 # 特定のページのデータの中身を確認する関数
 def check_df_page(df: pd.DataFrame, page_num: int) -> None:
     print(f"page_num: {page_num}")
-    # page_numのtext, size, between_lineを表示する
-    print(df[df["page_num"] == page_num][["text", "size", "between_line"]])
+    # page_numのデータを表示
+    print(df[df["page_no"] == page_num])
     print("\n")
 
 
@@ -38,32 +39,24 @@ if __name__ == "__main__":
     # データの中身を確認する
     check_df(df)
 
-    # 列幅が一定以上か否かを判定する列を追加する
-    df["width_is_over"] = df["between_line"].apply(
-        lambda x: x > settings.width_threshold
+    # charが" "の行を削除
+    df = df[df["char"] != " "]
+
+    df["height"] = df["y1"] - df["y0"]
+
+    df_text = df.groupby(["page_no", "block_no"])["char"].apply(lambda x: "".join(x))
+
+    df_size = df.groupby(["page_no", "block_no"])["height"].apply(
+        lambda x: trim_mean(x, 0.3)
     )
 
-    # between_lineがNaNの行はwidth_is_overをTrueにする
-    # (タイトルだけのページかもしれないから)
-    df.loc[df["between_line"].isnull(), "width_is_over"] = True
+    # df_textとdf_heightを結合する
+    df = pd.concat([df_text, df_size], axis=1)
 
-    # width_is_overがTrueの行のみを抽出する
-    df = df[df["width_is_over"] == True]
+    df_out = df[
+        (df["height"] > settings.min_font_size)
+        & (df["height"] < settings.max_font_size)
+    ]
 
-    # sizeが一定以上か否かを判定する列を追加する
-    df = df[df["size"] >= settings.font_size_threshold]
-
-    # width_is_overがTrueの行のみ表示する
-    print(df[["text", "page_num", "size"]])
-
-    # width_is_overがTrueの行のみCSVに出力する
-    df[["text", "page_num", "size", "between_line", "width_is_over"]].to_csv(
-        "tmp/split_point_candidate.csv", index=True
-    )
-
-    # split_point.csvというファイルを作成する
-    # すでに存在する場合は、作成しない
-    if not os.path.exists("tmp/split_point.csv"):
-        with open("tmp/split_point.csv", "w") as f:
-            # 1行目に列名を書き込む
-            f.write("split_point\n")
+    # split_point_candidate.csvを保存する
+    df_out[["height", "char"]].to_csv("tmp/split_point_candidate.csv", index=True)
