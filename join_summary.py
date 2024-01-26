dba72b2e2cc487e231b062a9d771ee2ea9a9f1ab
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2024-01-26 08:02:33 +0000

    Refactor join_summary.py to improve code readability and remove unnecessary variables and comments

--- join_summary.py
+++ join_summary.py
@@ -9,15 +9,6 @@ if __name__ == '__main__':
     # docxを新規作成
     doc = docx.Document()
 
-    # loop_split_rangeを初期化する
-    loop_split_range = df.at[0, 'split_range']
-    # titleを初期化する
-    title = df.at[0, 'title']
-
-    # docxにsplit_rangeを見出しとして追加する
-    doc.add_heading(title, level=1)
-
-    # dfをforループで処理する
     for index, row in df.iterrows():
         # summaryから呼び込むためのファイルパスに変更する
         summary_path = row['file_name'].replace('prompt', 'summary')
@@ -26,20 +17,9 @@ if __name__ == '__main__':
         with open(summary_path) as f:
             summary = f.read()
         
-        # その行のsplit_rangeがloop_split_rangeと異なる場合
-        if row['split_range'] != loop_split_range:
-            # docxに改ページを追加する
-            # doc.add_page_break()
-            
-            # loop_split_rangeを更新する
-            loop_split_range = row['split_range']
+        # docxに見出しを追加する
+        doc.add_heading(row['title'], level=1)
 
-            # titleを変数に格納する
-            title = row['title']
-
-            # docxにsplit_rangeを追加する
-            doc.add_heading(title, level=1)
-        
         # docxにsummaryを追加する
         doc.add_paragraph(summary)
     
