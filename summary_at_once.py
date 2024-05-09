654bff28800c4b2e6e26a54fb34655defced4875
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2024-05-09 20:31:30 +0000

    Refactor summary_at_once.py to use settings module for file name

--- summary_at_once.py
+++ summary_at_once.py
@@ -1,9 +1,10 @@
 import summary_all
 import os
+import settings
 
 if __name__ == "__main__":
-    # ファイル名を指定する(書き換える)
-    flie = "0000.txt"
+    # ファイル名を指定する
+    flie = settings.file_name
 
     # summaryディレクトリを作成する
     os.makedirs("summary", exist_ok=True)
